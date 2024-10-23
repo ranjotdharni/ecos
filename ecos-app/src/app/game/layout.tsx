84e0e5bac8c8cad18ee679009219259d4b8f60ed
Ranjot Dharni <ranjotdharni1@gmail.com>
2024-10-22 19:33:18 -0700

    ui confirm modal complete

--- ecos-app/src/app/game/layout.tsx
+++ ecos-app/src/app/game/layout.tsx
@@ -1,12 +1,29 @@
+'use client'
+
+import React, { useState } from "react"
+import ConfirmModal, { Confirm } from "../components/confirm/ConfirmModal"
 import NavBar from "../components/navbar/NavBar"
 
-export default async function Layout({ children } : { children: React.ReactNode }) {
+export const Context = React.createContext<{ setModal: (slug: Confirm) => void }>({
+    setModal: () => {}
+})
+
+export default function Layout({ children } : { children: React.ReactNode }) {
+
+    const [modalSlug, setModal] = useState<Confirm>({
+        title: '',
+        message: '',
+        question: '',
+        callback: () => {}
+    })
+
 
     return (
-        <>
+        <Context.Provider value={ { setModal: setModal } }>
+            <ConfirmModal slug={modalSlug} />
             {/* Always leave 5vh at top of each page for navbar */}
             <NavBar />
             {children}
-        </>
+        </Context.Provider>
     )
 }
