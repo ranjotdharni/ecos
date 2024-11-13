b139969fdad1660f71a49bbf8eb47a87561d1092
Ranjot Dharni <ranjotdharni1@gmail.com>
2024-11-13 06:01:00 -0800

    new congregation page ui complete

--- ecos-app/src/app/server/congregation.ts
+++ ecos-app/src/app/server/congregation.ts
@@ -1,6 +1,8 @@
 import { CITY_ICON, SETTLEMENT_ICON } from "@/customs/utils/constants"
 import { CongregationType } from "@/customs/utils/types"
 
+export const NEW_CONGREGATION_COST: number = 10000000
+
 export const CONGREGATION_TYPES: CongregationType[] = [
     {
         type: 0,
